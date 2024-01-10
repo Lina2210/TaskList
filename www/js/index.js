b2f614f5cbcad92e463982f2880fb99900c5bf03
linaramdez <liramdez@gmail.com>
2024-01-10 18:26:40 +0100

    TaskList delete and edit done

--- www/js/index.js
+++ www/js/index.js
@@ -29,9 +29,64 @@ function onDeviceReady() {
 }
 
  	
+$(document).on("pagecreate", "#homePage", function() {
+    //add element
+    $('#add').on("click", function() {
+        var list = $('[data-role="listview"]');
+        var newItemName = prompt("Ingrese el nombre del nuevo elemento:", "Nombre");
+        
+        if (newItemName !== null && newItemName !== "") {
+            var newListItem = $('<li><a href="#"><button class="deleteButton">X</button><button class="editButton">!</button>' + " " + newItemName + '</a></li>').appendTo(list);
+            list.listview('refresh');
+        }
+    });
+    //add buttons to all elements 
+    $('[data-role="listview"] li').each(function() {
+        var $this = $(this);
+        if (!$this.find('.deleteButton').length) {
+            var itemName = $this.find('a').text();
+            $this.html('<a href="#"><button class="deleteButton">X</button><button class="editButton">!</button>' + " " + itemName + '</a>');
+        }$('[data-role="listview"]').listview('refresh');
+    });
+  
 
-$( "#add" ).on( "click", function() {
     
-    $('ul[data-role="listview"]').append("<li>New Element.");
-    
-  } );
+    // delete element
+    $(document).on("click", ".deleteButton", function() {
+        var listItem = $(this).closest('li');
+        listItem.remove();
+        $('[data-role="listview"]').listview('refresh');
+    });
+
+    //edit element with prompt
+    /*$(document).on("click", ".editButton", function() {
+        var listItem = $(this).closest('li');
+        var newItemName = prompt("Ingrese el nuevo nombre:");
+        listItem.find('a').text("");
+        var buttonsHTML = '<button class="deleteButton">X</button><button class="editButton">!</button>';
+        listItem.find('a').prepend(buttonsHTML + " " + newItemName);
+        $('[data-role="listview"]').listview('refresh');
+    });*/
+
+    //edit element with input
+    $(document).on("click", ".editButton", function() {
+        var listItem = $(this).closest('li');
+        var newItem = $('<input type="text" placeholder="Nuevo nombre">').val(listItem.find('a').clone().children().remove().end().text());
+        var okButton = $('<button class="okButton">OK</button>');
+        listItem.find('a').text("").append(newItem, okButton);
+
+        $(document).on("click", ".okButton", function(){
+
+            var buttonsHTML = '<button class="deleteButton">X</button><button class="editButton">!</button>';
+            var newItemName = newItem.val()
+            // Reemplazar el texto con el input y los botones
+            listItem.find('a').empty().append(newItemName).prepend(buttonsHTML);
+            
+            // Refrescar la lista después de realizar los cambios
+            $('[data-role="listview"]').listview('refresh');
+        })
+        
+    });
+});
+
+  
